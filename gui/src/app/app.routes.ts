23ff46060860f9588fc7375cf7012e4c6ff94e08
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-11-07 15:23:35 +0200

    layout elements in work

--- gui/src/app/app.routes.ts
+++ gui/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CurrentWorkoutComponent } from './current-workout/current-workout.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     { 
         path: '', 
         component: HomeComponent
+    }, 
+    { 
+        path: 'current-workout',
+        component: CurrentWorkoutComponent
+    }, 
+    { 
+        path: 'profile', 
+        component: ProfileComponent
     }
 ];
---
+++ gui/src/app/current-workout/current-workout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-workout',
+  standalone: true,
+  imports: [],
+  templateUrl: './current-workout.component.html',
+  styleUrl: './current-workout.component.scss'
+})
+export class CurrentWorkoutComponent {
+
+}
